79e771de346c899ef6ae05e982eab067a8206ce6
NanaEffah <noeffah@st.knust.edu.gh>
2025-07-13 02:03:31 +0000

    Add project files.

---
+++ Data/Migrations/20250713012849_AddDescriptionAndPriceToBook.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndPriceToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Book");
+        }
+    }
+}
